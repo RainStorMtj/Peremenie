d5065437e3ace07463f2f3454318edf8309c567e
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-10-18 22:40:40 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,5 @@
+kol_DZ = 12
+kol_chasov = 1.5
+kurs = 'Python'
+vremya = kol_chasov / kol_DZ
+print('Курс: ' + kurs + ',', 'всего задач: ' + str(kol_DZ) + ',', 'затрачено часов: ' + str(kol_chasov) + ',', 'среднее время выполнения: ' + str(vremya) + ' часа.')
